6ee7ded8bd0be677dd2f152fd1d7b089b4221190
Ritik Sinha <ritik210798@gmail.com>
2025-02-18 17:31:03 +0530

    OCP Explained

--- O/codeSnippet.java
+++ O/codeSnippet.java
@@ -11,6 +11,12 @@ class AreaCalculator {
     }
 }
 
+
+
+//Problem: Imagine you want to add a new shape, like a Circle, and calculate its area. You would have to modify the AreaCalculator class. You'd likely add an else if or a switch statement to handle the Circle type.
+
+//The AreaCalculator is now modified every time you add a new shape. The OCP states that classes should be open for extension but closed for modification. You're directly changing the AreaCalculator's code to accommodate new shapes. 
+
 //Adhering to OCP
 
 interface Shape {
@@ -34,5 +40,9 @@ class Circle implements Shape {
         return Math.PI * radius * radius;
     }
 }
+
+//The introduction of the Shape interface is the key.  Now, both Rectangle and Circle implement this interface.  This means they both have a calculateArea() method.
+
+// How it adheres to OCP:  You can now create a new AreaCalculator (or modify an existing one slightly if needed) that works with any Shape:
 
 //Now, you can add new shapes (e.g., Circle) without modifying the AreaCalculator class.
